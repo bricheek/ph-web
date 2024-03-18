b5d6810fc9e322b757a71c76d1339f6882a67770
bricheek <bricheek@gmail.com>
2024-03-17 21:57:01 -0400

    secrets

--- client/src/firebase.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    apiKey: "AIzaSyD_Utt2UBIZG0NGPoJ7DNj-qpoAAeFZkyg",
-    authDomain: "phfb-7aa87.firebaseapp.com",
-    projectId: "phfb-7aa87",
-    storageBucket: "phfb-7aa87.appspot.com",
-    messagingSenderId: "343197799754",
-    appId: "1:343197799754:web:db725c5eb86777774f1db3",
-    measurementId: "G-TWZYRYW8C4",
-  };
